20eceb21e541d42f7b2b9a773cffa6313fdd32d6
vinitathopte2603 <vinitathopte1@outlook.com>
2020-05-02 16:13:58 +0530

    [Vinita]ADDED:GambleFunctionToBet

--- gamblerSimulation.sh
+++ gamblerSimulation.sh
@@ -3,6 +3,22 @@
 var="Welcome to Gambler Simulation"
 echo "$var"
 
-INITIAL_STAKE=100
-DAY=1
-BET=1
+INITIAL_STAKE=100;
+DAY=1;
+BET=1;
+WIN=1;
+LOSS=0;
+finalStake=INITIAL_STAKE;
+
+function Gamble(){
+
+	randomCheck=$((RAMDOM%2))
+	if [[ $randomCheck -eq $1 ]]
+	then
+		finalStake=$(( $finalStake + 1 ))
+		return $finalStake
+	else
+		finalStake=$(( $finalStake - 1 ))
+		return $finalStake
+	fi
+}
