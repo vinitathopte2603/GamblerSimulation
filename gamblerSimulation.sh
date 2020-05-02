8287309cfc05018bfbf2fb0c21eb082c07b2c5df
vinitathopte2603 <vinitathopte1@outlook.com>
2020-05-02 19:26:42 +0530

    [Vinita]ADDED:CalculateTheTotalAmountAfter20DaysOfGambling

--- gamblerSimulation.sh
+++ gamblerSimulation.sh
@@ -9,12 +9,16 @@ BET=1;
 WIN=1;
 LOSS=0;
 PERCENTAGE_STAKE=50;
+DAYS=20;
 finalStake=$INITIAL_STAKE;
 winningCondition=$(( $INITIAL_STAKE + $(( $INITIAL_STAKE / 100 * $PERCENTAGE_STAKE )) ))
 loosingCondition=$(( $INITIAL_STAKE - $(( $INITIAL_STAKE / 100 * $PERCENTAGE_STAKE )) ))
 
 
 function Gamble(){
+	for (( dayCount=1; dayCount<DAYS; dayCount++ ))
+	do
+		finalStake=$INITIAL_STAKE;
 	while [ $finalStake -lt $winningCondition ] && [ $finalStake -gt $loosingCondition ]
 	do
  		randomCheck=$(( RANDOM % 2 ))
@@ -26,6 +30,9 @@ function Gamble(){
 			finalStake=$(( $finalStake - 1 ))
 
 		fi
+	done
+	totalAmount=$(( $finalStake - $INITIAL_STAKE ))
+	totalGamblerAmount=$(( $TotalGamblerAmount + $totalAmount ))
 	done
 	echo "Gambler resigned for the day"
 }
