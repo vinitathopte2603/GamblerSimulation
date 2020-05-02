e9cfda9f83be57154d2be5586015fad00d1eaf9e
vinitathopte2603 <vinitathopte1@outlook.com>
2020-05-02 20:41:20 +0530

    [Vinita]ADDED:LossOrWinForEveryMonth

--- gamblerSimulation.sh
+++ gamblerSimulation.sh
@@ -10,10 +10,15 @@ WIN=1;
 LOSS=0;
 PERCENTAGE_STAKE=50;
 DAYS=20;
+winCount=0;
+lossCount=0;
+winTotalAmount=0;
+lossTotalAmount=0;
 finalStake=$INITIAL_STAKE;
 winningCondition=$(( $INITIAL_STAKE + $(( $INITIAL_STAKE / 100 * $PERCENTAGE_STAKE )) ))
 loosingCondition=$(( $INITIAL_STAKE - $(( $INITIAL_STAKE / 100 * $PERCENTAGE_STAKE )) ))
 
+declare -A gambledAmount
 
 function Gamble(){
 	for (( dayCount=1; dayCount<DAYS; dayCount++ ))
@@ -25,16 +30,29 @@ function Gamble(){
 
 		if [[ $randomCheck -eq $1 ]]
 		then
-			finalStake=$(( $finalStake + 1 ))
+			finalStake=$(( $finalStake + $BET ))
 		else
-			finalStake=$(( $finalStake - 1 ))
+			finalStake=$(( $finalStake - $BET ))
 
 		fi
 	done
 	totalAmount=$(( $finalStake - $INITIAL_STAKE ))
+	gambledAmount[$dayCount]=$totalAmount
 	totalGamblerAmount=$(( $TotalGamblerAmount + $totalAmount ))
 	done
 	echo "Gambler resigned for the day"
+
+	for data in "${!gambledAmount[@]}"
+	do
+		if [[ ${gambledAmount[$data]} -lt $LOSS ]]
+		then
+			lossCount=$(( $lossCount + 1 ))
+			lossTotalAmount=$(( $lossTotalAmount + ${gambledAmount[$data]} ))
+		else
+			winCount=$(( $winCount + 1 ))
+			winTotalAmount=$(( $winTotalAmount + ${gambledAmount[$data]} ))
+		fi
+	done
 }
 
 Gamble $(($WIN))
